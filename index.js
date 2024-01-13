27bd3ac2a816ae9d794a4151f2eb816fab05dee6
UoOsSe <143217955+ousey-ousey@users.noreply.github.com>
2024-01-13 06:26:21 +0200

    final

--- index.js
+++ index.js
@@ -13,17 +13,17 @@ async function checkweather(city){
         document.querySelector('#speed').innerHTML = Math.round(data.wind.speed)  + ` Km/h`
         document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + ` °C`
         if(data.weather[0].main == "Clouds"){
-            weatherIcon.src = "/images/clouds.png"
+            weatherIcon.src = "images/clouds.png"
         }else if (data.weather[0].main == "Clear"){
-            weatherIcon.src = "/images/clear.png"
+            weatherIcon.src = "images/clear.png"
         }else if (data.weather[0].main == "Rain"){
-            weatherIcon.src = "/images/rain.png"
+            weatherIcon.src = "images/rain.png"
         }else if (data.weather[0].main == "Drizzle"){
-            weatherIcon.src = "/images/drizzle.png"
+            weatherIcon.src = "images/drizzle.png"
         }else if (data.weather[0].main == "Mist"){
-            weatherIcon.src = "/images/mist.png"
+            weatherIcon.src = "images/mist.png"
         }else if (data.weather[0].main == "Snow"){
-            weatherIcon.src = "/images/snow.png"
+            weatherIcon.src = "images/snow.png"
         }
         document.querySelector(".weather").style.display = "block"
         document.querySelector(".erro p").style.display = "none"
